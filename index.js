f93d79e5d0c901a5f52e5fa81f9b3f68bafc1bfd
kaichiro <kcfg23@gmail.com>
2019-05-17 17:27:18 -0300

    Refatorando código

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-require('dotenv')
+require('dotenv/config')
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.send('DevShop')
+    res.render('home')
 })
 
 app.listen(port, err => {
